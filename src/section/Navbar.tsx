8a509ff62794c615d7fb4ad69c9ac33f55db36bf
Prakkks <tprakrity@gmail.com>
2025-05-21 20:50:49 +0545

    SOmething better than nothing

--- src/section/Navbar.tsx
+++ src/section/Navbar.tsx
@@ -2,7 +2,7 @@
 const Navbar = () => {
   return (
     <header className="fixed text-white top-0 left-0 right-0 z-50 bg-black/90 ">
-        <div className=" max-w-7xl mx-auto"></div>
+        <div className=" max-w-7xl mx-auto">Hi Prakriti</div>
     </header>
   )
 }
