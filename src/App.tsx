64256750581a0117da38464f37363c73d9577f4e
Prakkks <tprakrity@gmail.com>
2025-06-12 23:33:41 +0545

    D

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import About from "./section/About";
 import CertificationIntern from "./section/CertificationIntern";
 import Contact from "./section/Contact";
@@ -8,41 +9,62 @@ import Navbar from "./section/Navbar";
 import Project from "./section/Project";
 import Skills from "./section/Skills";
 
-
-
 function App() {
-  return (
-    
-    <main className="  " id="home">
-     <div className="bg-[url('/assets/herobg.png')] bg-cover bg-center max-w-full  ">
-     <div className="max-w-7xl mx-auto">
-     <Navbar />
-     <HeroSection />
-     </div>
-     </div>
-     <div className="max-w-7xl mx-auto">
-     {/* <Hero /> */}
-     <About />
-      <Skills />
-     </div>
-     <div className="bg-[url('/assets/herobg-flip.png')]  bg-cover bg-center max-w-full  ">
-     <div className="max-w-7xl mx-auto">
-     <Education />
-     </div>
-     </div>
-     <div className="max-w-7xl mx-auto">
-      <CertificationIntern />
-      <Project />
-    
-     <Contact />
-     <Footer />
-     </div>
-    </main>
-   
-    
-  );
+  // useEffect(() => {
+  //   const handleRightClick = (e:any) => e.preventDefault();
+  //   const handleCopy = (e:any) => {
+  //     e.preventDefault();
+  //     alert("Copying is disabled.");
+  //   };
+  //   const handleKeyDown = (e:any) => {
+  //     if (
+  //       (e.ctrlKey && (e.key === "c" || e.key === "u")) ||
+  //       e.key === "F12"
+  //     ) {
+  //       e.preventDefault();
+  //       alert("This shortcut is disabled.");
+  //     }
+  //   };
 
+  //   document.addEventListener("contextmenu", handleRightClick);
+  //   document.addEventListener("copy", handleCopy);
+  //   document.addEventListener("keydown", handleKeyDown);
+
+  //   return () => {
+  //     document.removeEventListener("contextmenu", handleRightClick);
+  //     document.removeEventListener("copy", handleCopy);
+  //     document.removeEventListener("keydown", handleKeyDown);
+  //   };
+  // }, []);
+
+  return (
+    <main className="select-none" id="home">
+      <div className="bg-[url('/assets/herobg.png')] bg-cover bg-center max-w-full">
+        <div className="max-w-7xl mx-auto">
+          <Navbar />
+          <HeroSection />
+        </div>
+      </div>
+
+      <div className="max-w-7xl mx-auto">
+        <About />
+        <Skills />
+      </div>
+
+      <div className="bg-[url('/assets/herobg-flip.png')] bg-cover bg-center max-w-full">
+        <div className="max-w-7xl mx-auto">
+          <Education />
+        </div>
+      </div>
+
+      <div className="max-w-7xl mx-auto">
+        <CertificationIntern />
+        <Project />
+        <Contact />
+        <Footer />
+      </div>
+    </main>
+  );
 }
 
 export default App;
-
